4417b72ec040578ccb8060b5a52718450144f22f
KairuMaina <george.kairu@moringaschool.com>
2024-11-29 14:55:30 +0300

    debugged the indextest.js, completed the control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!"; // Free ride for distances 400 feet or less
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks."; // $20 for distances between 400 and 2000 feet
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks."; // $30 for distances between 2000 and 2500 feet
+  } else {
+    return "No can do."; // Refuse rides greater than 2500 feet
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go."; // Use ternary operator
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+
